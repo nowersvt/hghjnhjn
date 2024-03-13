191e0558ea122a6eef1582138478e64c190a3ee9
Your Name <you@example.com>
2024-03-13 03:32:30 +0100

    l

---
+++ assessment-ui/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="container">
+        <h1 class="title">Hello World!</h1>
+    </div>
+</template>
+
+<style scoped>
+.title {
+    text-align: center;
+    font-size: 5rem;
+}
+</style>
